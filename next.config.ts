e7fe10832026ad77e7079e9d3d316d269c5b0170
JayWood <jjwood2004@gmail.com>
2025-03-15 21:37:32 -0400

    Initial

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+  assetPrefix: isProd ? "/webtools/" : "",
+  basePath: isProd ? "/webtools/" : "",
+  trailingSlash: true
+};
+
+export default nextConfig;
